775bf118d54024b1f2760e3363525c44c5ca3efb
nguyenbachduyhoang <nguyenduyhoang1494@gmail.com>
2025-12-15 14:58:45 +0700

    layout 01

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -974,8 +974,6 @@ function AdminDashboard() {
                   ]}
                   cx="50%"
                   cy="50%"
-                  labelLine={false}
-                  label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
                   outerRadius={100}
                   fill="#8884d8"
                   dataKey="value"
